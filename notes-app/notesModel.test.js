2a33221fb7bffe9bc10d424c7cb9c6ef85ebfd82
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-01-04 14:19:30 +0000

    Adds test for setNotes

--- notes-app/notesModel.test.js
+++ notes-app/notesModel.test.js
@@ -1,14 +1,16 @@
 const NotesModel = require('./notesModel');
 
 describe(NotesModel, () => {
+  let notesModel;
+  beforeEach(() => {
+    notesModel = new NotesModel();
+  });
+  
   it('creates an empty array', () => {
-    const notesModel = new NotesModel();
-
     expect(notesModel.getNotes()).toEqual([]);
   });
 
   it('adds two notes', () => {
-    const notesModel = new NotesModel();
     notesModel.addNote('Note 1');
     notesModel.addNote('Note 2');
 
@@ -16,11 +18,16 @@ describe(NotesModel, () => {
   });
 
   it('reset empties notes array', () => {
-    const notesModel = new NotesModel();
     notesModel.addNote('Note 1');
     notesModel.addNote('Note 2');
     notesModel.reset();
 
     expect(notesModel.getNotes()).toEqual([]);
+  });
+
+  it('setNotes replaces the notes with the given array', () => {
+    notesModel.addNote('Note 1');
+    notesModel.setNotes(['Note 2', 'Note 3']);
+    expect(notesModel.getNotes()).toEqual(['Note 2', 'Note 3']);
   });
 });
